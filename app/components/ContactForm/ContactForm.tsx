62b89e5ae7e7d331247fbffcdc33ac96ccfb5e9e
CharlesSquirel <karol.wiewiorka@gmail.com>
2024-01-07 15:17:26 +0100

    feat: logic for contact form

--- app/components/ContactForm/ContactForm.tsx
+++ app/components/ContactForm/ContactForm.tsx
@@ -1,38 +1,58 @@
+"use client";
 import style from "./ContactForm.module.scss";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { validationSchema } from "@/app/utils/validation";
+
+interface FormInput {
+  name: string;
+  email: string;
+  message: string;
+}
+
 const ContactForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInput>({
+    mode: "all",
+    resolver: yupResolver(validationSchema),
+  });
+
+  const onSubmit: SubmitHandler<FormInput> = (data) => console.log(data);
+
   return (
-    <form className={style.container}>
+    <form className={style.container} onSubmit={handleSubmit(onSubmit)}>
       <h3 className={style.title}>Napisz do mnie!</h3>
       <div className={style.inputBox}>
         <label htmlFor="name" className={style.labelHidden}>
           Imię
         </label>
-        <input type="text" name="name" placeholder="Imię" className={style.input} id="name" />
+        <input type="text" placeholder="Imię" className={style.input} id="name" {...register("name")} />
         <p role="alert" className={style.errorMessage}>
-          Invalid input
+          {errors.name?.message}
         </p>
       </div>
       <div className={style.inputBox}>
         <label htmlFor="email" className={style.labelHidden}>
           Email
         </label>
-        <input type="text" name="name" placeholder="Email" className={style.input} id="email" />
+        <input type="text" placeholder="Email" className={style.input} id="email" {...register("email")} />
         <p role="alert" className={style.errorMessage}>
-          Invalid input
+          {errors.email?.message}
         </p>
       </div>
       <div className={style.textareaContainer}>
         <label htmlFor="message" className={style.labelHidden}>
-          Treść wiadomości
+          {errors.email?.message}
         </label>
-        <textarea name="message" id="message" className={style.textarea} placeholder="Treść wiadomości"></textarea>
+        <textarea id="message" className={style.textarea} placeholder="Treść wiadomości" {...register("message")}></textarea>
         <p className={style.errorMessage} role="alert">
-          Invalid input
+          {errors.message?.message}
         </p>
       </div>
-      <button type="submit" className={style.btn}>
-        Wyślij
-      </button>
+      <input type="submit" className={style.btn} />
     </form>
   );
 };
---
+++ app/utils/validation.ts
@@ -0,0 +1,9 @@
+import * as Yup from "yup";
+
+export const validationSchema = Yup.object({
+  name: Yup.string()
+    .required("To pole jest wymagane")
+    .matches(/^[a-zA-Z]+$/gi, "Imię nie powinno zawierać cyfr ani znaków specjalnych"),
+  email: Yup.string().required("To pole jest wymagane").email("Niepoprawny email"),
+  message: Yup.string().required("To pole jest wymagane"),
+});
