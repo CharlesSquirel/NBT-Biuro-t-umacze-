5d9796d5a325176e26072bef414ef49e41b0f1d6
CharlesSquirel <karol.wiewiorka@gmail.com>
2024-01-08 20:03:19 +0100

    feat: separate input fields to reusable components, add input fo title email

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 import Mail from "nodemailer/lib/mailer";
 
 export async function POST(request: NextRequest) {
-  const { email, name, message } = await request.json();
+  const { email, name, message, title } = await request.json();
   const transport = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -15,7 +15,7 @@ export async function POST(request: NextRequest) {
   const mailOptions: Mail.Options = {
     from: process.env.MY_EMAIL,
     to: process.env.MY_EMAIL,
-    subject: `Message from ${name} (${email})`,
+    subject: `Wiadomość od ${name} (${email}). Tytuł: ${title}`,
     text: message,
   };
 
--- app/utils/validation.ts
+++ app/utils/validation.ts
@@ -5,5 +5,6 @@ export const validationSchema = Yup.object({
     .required("To pole jest wymagane")
     .matches(/^[a-zA-Z]+$/gi, "Imię nie powinno zawierać cyfr ani znaków specjalnych"),
   email: Yup.string().required("To pole jest wymagane").email("Niepoprawny email"),
+  title: Yup.string().required("To pole jest wymagane"),
   message: Yup.string().required("To pole jest wymagane"),
 });
