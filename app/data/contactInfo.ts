8b2b88ef4b87714c2a26d81079864af9de6715e7
Karol <108272338+CharlesSquirel@users.noreply.github.com>
2024-03-25 12:38:58 +0100

    Update contactInfo.ts

--- app/data/contactInfo.ts
+++ app/data/contactInfo.ts
@@ -8,5 +8,5 @@ interface ContactProps {
 }
 
 export const contactInfo: ContactProps[] = [
-  { addressPrimary: "ul. Strzeszewskiego 25/23", addressSecondary: "20-153 Lublin", nip: "712-246-78-95", regon: "060567875", phone: "516 169 233", email: "nbt.tlumaczenia@gmail.com" },
+  { addressPrimary: "ul. Strzeszewskiego 25/23", addressSecondary: "20-153 Lublin", nip: "712-246-78-95", regon: "060567875", phone: "+ 48 516 169 233", email: "nbt.tlumaczenia@gmail.com" },
 ];
