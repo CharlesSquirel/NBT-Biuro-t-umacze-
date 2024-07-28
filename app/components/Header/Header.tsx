59bd59a51fa5b1446a93b4f6471515c586731c4d
CharlesSquirel <karol.wiewiorka@gmail.com>
2024-07-28 17:57:04 +0200

    Refactor: Change logo and main red color hex

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,39 +1,89 @@
-"use client";
-import { useState } from "react";
-import Image from "next/image";
-import Link from "next/link";
-import style from "./Header.module.scss";
-import globalStyle from "style/Globals.module.scss";
-import NavDesktop from "@/components/NavDesktop/NavDesktop";
-import NavMobile from "@/components/NavMobile/NavMobile";
-import SecondaryMobileHeader from "@/components/SecondaryMobileHeader/SecondaryMobileHeader";
-import logoImg from "../../../public/logo.svg";
+'use client';
+import { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import style from './Header.module.scss';
+import globalStyle from 'style/Globals.module.scss';
+import NavDesktop from '@/components/NavDesktop/NavDesktop';
+import NavMobile from '@/components/NavMobile/NavMobile';
+import SecondaryMobileHeader from '@/components/SecondaryMobileHeader/SecondaryMobileHeader';
+import logoImg from '../../../public/logo.png';
 
 const Header: React.FC = () => {
-  const [isNavMobileOpen, setIsNavMobileOpen] = useState(false);
-  const handleNavMobileOpen = (): void => {
-    setIsNavMobileOpen(!isNavMobileOpen);
-  };
+  const [
+    isNavMobileOpen,
+    setIsNavMobileOpen,
+  ] = useState(false);
+  const handleNavMobileOpen =
+    (): void => {
+      setIsNavMobileOpen(
+        !isNavMobileOpen
+      );
+    };
   return (
     <>
-      <header className={style.container}>
-        <div className={style.logoContainer}>
+      <header
+        className={style.container}
+      >
+        <div
+          className={
+            style.logoContainer
+          }
+        >
           <Link href='/'>
-            <Image src={logoImg} alt='Logo' className={style.logo} />
+            <Image
+              src={logoImg}
+              alt='Logo'
+              className={style.logo}
+            />
           </Link>
-          <div className={style.textContainer}>
-            <div className={style.titleContainer}>
-              <h1 className={style.title}>
-                <span className={globalStyle.capitalizeBold}>NBT</span> Anna Król
+          <div
+            className={
+              style.textContainer
+            }
+          >
+            <div
+              className={
+                style.titleContainer
+              }
+            >
+              <h1
+                className={style.title}
+              >
+                <span
+                  className={
+                    globalStyle.capitalizeBold
+                  }
+                >
+                  NBT
+                </span>{' '}
+                Anna Król
               </h1>
-              <h2 className={style.subtitle}>Biuro Tłumaczeń Języka Angielskiego</h2>
+              <h2
+                className={
+                  style.subtitle
+                }
+              >
+                Biuro Tłumaczeń Języka
+                Angielskiego
+              </h2>
             </div>
-            <p className={style.text}>Daj sobie przetłumaczyć!</p>
+            <p className={style.text}>
+              Daj sobie przetłumaczyć!
+            </p>
           </div>
         </div>
-        <NavDesktop onClick={handleNavMobileOpen} />
+        <NavDesktop
+          onClick={handleNavMobileOpen}
+        />
 
-        {isNavMobileOpen && <NavMobile onClick={handleNavMobileOpen} />}
+        {isNavMobileOpen && (
+          <NavMobile
+            onClick={
+              handleNavMobileOpen
+            }
+          />
+        )}
       </header>
       <SecondaryMobileHeader />
     </>
